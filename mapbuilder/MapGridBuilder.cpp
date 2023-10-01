3380d46dffe927df8470b4b0fb9fd77dd37e6022
RedRob0 <0nly4bu155n355@gmail.com>
2023-10-01 15:28:49 -0700

    initial functions for MapGridBuilder

--- mapbuilder/MapGridBuilder.cpp
+++ mapbuilder/MapGridBuilder.cpp
@@ -1,5 +1,8 @@
 #include "MapGrid.cpp"
+#include "GameObjList.cpp"
 #include "filePairReader.cpp"
+#include <utility>
+#include <vector>
 /*
 	PLACEHOLDER, this object handles populating MapGrid objects 
 	with game objects of many varying types.
@@ -11,5 +14,19 @@
 
 class MapGridBuilder {
 private:
+	//FilePairReader initializes here, only MapGridBuilder uses it
+	std::vector<std::tuple<MapGrid, GameObjList>> preparedLevels;
+	std::tuple<MapGrid, GameObjList> currentLevel;
+public:
+	//load levels according to a list of level names
+	std::vector<std::tuple<MapGrid, GameObjList>> loadLevels(std::list<std::string> levelList) {}
 
+	//build a level according to given level name by calling functions from the FilePairReader
+	std::tuple<MapGrid, GameObjList> buildLevel(std::string levelName) {}
+
+	//unload the active currentLevel and replace it with a tuple from our preparedLevels grabbed by name
+	void loadlevel(std::string levelName) {}
+
+	//clear preparedLevels and currentLevel
+	void clear() {}
 };
