f6ff790761a5d031c38316f1f761d247a4213b3b
RedRob0 <0nly4bu155n355@gmail.com>
2023-10-08 17:13:15 -0700

    GameObjList now implements GameObj

--- gamemaster/GameObjList.cpp
+++ gamemaster/GameObjList.cpp
@@ -20,9 +20,15 @@ public:
 	// Initialization function, empty
 	GameObjList() {}
 
+	// Initialization function, when given a list of GameObjects
+	GameObjList(std::vector<GameObj>) { serializeList(); }
+
 	//placeholder adds a GameObj to data
 	void addGameObj(GameObj obj) {
 		this->data.push_back(obj);
+		//serializeList() is currently too costlty to do after every added object,
+		//once the function is improved it can be called in addGameObj
+		//serializeList();
 	}
 
 	//returns # of objects within this GameObjList (use in conjunction with below function to loop
@@ -38,6 +44,8 @@ public:
 		}
 	}
 
+	//grabs an object from data by its serializedName
+
 	//removes an object from data at a specific index
 	void deleteGameObj(int index) {
 		if (index < this->size()) {
@@ -48,5 +56,14 @@ public:
 	//returns the list of game objects
 	std::vector<GameObj> getGameObjList() {
 		return this->data;
+	}
+
+	//applies serialized names to all objects
+	//this implementation is to be improved
+	void serializeList() {
+		for (int i = 0; i < data.size(); i++) {
+			std::string serialName = data[i].gameName + std::to_string(i);
+			data[i].setSerializedName(serialName);
+		}
 	}
 };
