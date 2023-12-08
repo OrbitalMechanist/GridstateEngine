ffa9239f9be612f2ebb1d4dfed1512f98c94a90f
AnthonyUch <60726950+AnthonyUch@users.noreply.github.com>
2023-12-08 00:19:44 -0800

    Added audio component

---
+++ headers/ecs/components/AudioComponent.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef AUDIOCOMPONENT_H
+#define AUDIOCOMPONENT_H
+#include "audio/SoundSource.h"
+// attack damage and range
+struct AudioComponent {
+    SoundSource source;
+};
+
+#endif 
